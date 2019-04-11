91c819854b43b9ca7376d8251e3ea6c48a0da949
班子涵 <1147092872@qq.com>
2019-04-11 09:53:55 +0800

    init

---
+++ src/main/java/com/poison/shiro/common/error/CodeException.java
@@ -0,0 +1,13 @@
+package com.poison.shiro.common.error;
+
+public class CodeException extends RuntimeException {
+    private int code;
+    public CodeException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
